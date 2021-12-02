3c4f39169e368aee546c5117271c8036f51e827a
Andranik Khandanyan <andranikkhan@gmail.com>
2021-12-02 18:14:28 +0400

    Implement Singly Linked List

--- src/main/java/am/studygarage/datastructures/Main.java
+++ src/main/java/am/studygarage/datastructures/Main.java
@@ -2,6 +2,5 @@ package am.studygarage.datastructures;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
---
+++ src/main/java/am/studygarage/datastructures/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,112 @@
+package am.studygarage.datastructures.linkedlist;
+
+public class SinglyLinkedList<T> {
+
+    private Node<T> head;
+    private Node<T> tail;
+
+    private int size;
+
+    public SinglyLinkedList() {
+
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public T first() {
+        if (isEmpty()) {
+            return null;
+        }
+        return this.head.getElement();
+    }
+
+    public T last() {
+        if (isEmpty()) {
+            return null;
+        }
+        return this.tail.getElement();
+    }
+
+    public void addFirst(T obj) {
+        head = new Node<>(obj, head);
+        if (isEmpty()) {
+            tail = head;
+        }
+
+        size++;
+    }
+
+    public void addLast(T obj) {
+        Node<T> newest = new Node<>(obj, null);
+        if (isEmpty()) {
+            head = newest;
+        } else {
+            tail.setNext(newest);
+        }
+        tail = newest;
+
+        size++;
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        T rv = head.getElement();
+        head = head.getNext();
+
+        size--;
+
+        if (isEmpty()) {
+            tail = null;
+        }
+
+        return rv;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder()
+                .append("[");
+        Node<T> node = head;
+        while (true) {
+            builder.append(node.getElement());
+            if (node.getNext() == null) {
+                break;
+            }
+            builder.append(",").append(" ");
+            node = node.getNext();
+        }
+        builder.append("]");
+        return builder.toString();
+    }
+
+    private static class Node<T> {
+        private final T element;
+        private Node<T> next;
+
+        public Node(T element, Node<T> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public T getElement() {
+            return element;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+    }
+}
